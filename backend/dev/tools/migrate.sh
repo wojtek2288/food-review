7033839c670a069383b3cfba71e4cb2d5840e2a4
Wojciech Klusek <wojciech.klusek@leancode.pl>
2022-10-15 15:01:42 +0200

    update dev

--- backend/dev/tools/migrate.sh
+++ backend/dev/tools/migrate.sh
@@ -4,8 +4,12 @@ set -e
 source /app/code/dev/config/config.sh
 echo "Config applied"
 
-echo "Migrating"
+echo "Migrating CoreDbContext"
 dotnet ef --startup-project ./src/Apps/FoodReview.Api/FoodReview.Api.csproj \
---project ./src/Core/FoodReview.Core.Services/FoodReview.Core.Services.csproj database update
+--project ./src/Core/FoodReview.Core.Services/FoodReview.Core.Services.csproj database update --context CoreDbContext
+
+echo "Migrating PersistedGrantDbContext"
+dotnet ef --startup-project ./src/Apps/FoodReview.Api/FoodReview.Api.csproj \
+--project ./src/Core/FoodReview.Core.Services/FoodReview.Core.Services.csproj database update --context PersistedGrantDbContext
 
 echo "All done"
