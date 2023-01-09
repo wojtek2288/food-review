be098eab2ff3c96cd57d059b37a72607b66f343e
Wojciech Klusek <wojciech.klusek@leancode.pl>
2023-01-09 14:26:49 +0100

    Add additional properties to user review

---
+++ backend/src/Core/FoodReview.Core.Contracts/Shared/RestaurantReviewDTO.cs
@@ -0,0 +1,11 @@
+namespace FoodReview.Core.Contracts.Shared;
+
+public class RestaurantReviewDTO
+{
+    public Guid ReviewId { get; set; }
+    public Guid RestaurantId { get; set; }
+    public string Name { get; set; } = default!;
+    public string ImageUrl { get; set; } = default!;
+    public double Rating { get; set; }
+    public string? Description { get; set; }
+}
